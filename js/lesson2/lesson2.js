ec5accd8f96be0405ba05f888df00a8a8258f7b7
Mykola Sadlii <mykolac08@gmail.com>
2026-03-04 21:44:54 +0000

    hw3

--- js/lesson2/lesson2.js
+++ js/lesson2/lesson2.js
@@ -15,3 +15,21 @@ window.addEventListener("mousemove",_.debounce((event)=>{
     boxRef.style.top = event.clientY + "px";
     boxRef.style.left = event.clientX + "px";
 },50));
+
+//  <!-- 1 -->
+
+//     <div class="slider">
+//       <input type="range" min="1" max="100" value="50" class="slider__input" />
+//       <img
+//         src="https://tse4.mm.bing.net/th/id/OIP.6vcGLGrmyfyqGRO77duxLwHaLa?rs=1&pid=ImgDetMain&o=7&rm=3"
+//         class="slider__image"
+//         alt="image"
+//       />
+//     </div>
+
+//     <!-- 2 -->
+
+//     <div id="box"></div>
+    
+//     <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.23/lodash.min.js"></script>
+//     <script type="module" src="./js/lesson2/lesson2.js"></script>
---
+++ js/lesson3/lesson3.js
@@ -0,0 +1,12 @@
+const imageObserver = new IntersectionObserver((elem,observer)=>{
+    elem.forEach((img)=>{
+        if (img.isIntersecting) {
+            const paths = img.target.dataset.src;
+            img.target.src = paths;
+            img.target.style.transform = "scale(1)"
+            observer.unobserve(img.target)
+        }
+    })
+},{});
+const allImagesArrayRef = document.querySelectorAll("img");
+allImagesArrayRef.forEach((item)=>imageObserver.observe(item))
